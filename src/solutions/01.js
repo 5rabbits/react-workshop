519c9408a08c70c24f8511e86610d18dc24ec9be
Rodrigo Díaz <rdiazv89@gmail.com>
2018-05-16 19:07:23 -0400

    Adds a comment to all solutions

--- src/solutions/01.js
+++ src/solutions/01.js
@@ -1,3 +1,52 @@
+/**
+ * 🚨 ATENCIÓN: Está es la solución a un problema, no la veas a menos que
+ * hayas podido resolverlo por ti mismo.
+ *
+ * Si necesitas orientación no tengas miedo de preguntar, o si lo prefieres
+ * puedes trabajar en conjunto con un compañero para que se apoyen mutuamente,
+ * pero si miras y copias la solución a los problemas no le sacarás mucho
+ * provecho al workshop, así que no lo hagas 😉
+ */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 import React from 'react'
 
 const HelloWorld = () => <div>Hola mundo</div>
