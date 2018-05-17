cb100af030c5d736f90c3d7c93189f2559cde40a
Rodrigo Díaz <rdiazv89@gmail.com>
2018-05-16 21:14:13 -0400

    Watcher

--- src/reporter.js
+++ src/reporter.js
@@ -1,6 +1,8 @@
 const jest = require('jest')
 const path = require('path')
 const createJestConfig = require('react-scripts/scripts/utils/createJestConfig')
+const fs = require('fs')
+const debounce = require('lodash.debounce')
 
 const defaultJestConfig = createJestConfig(
   relativePath => path.resolve('node_modules/react-scripts', relativePath),
@@ -12,21 +14,29 @@ defaultJestConfig.transform['^.+\\.(js|jsx|mjs)$'] = '<rootDir>/node_modules/bab
 
 process.env.NODE_ENV = 'test'
 
-jest.runCLI({
-  ...defaultJestConfig,
-  silent: true,
-  env: 'jsdom',
-  reporters: [],
-}, [
-  path.resolve('.')
-], ({ testResults }) => {
-  const results = {}
+const runTests = debounce(exercise => {
+  jest.runCLI({
+    ...defaultJestConfig,
+    silent: true,
+    env: 'jsdom',
+    reporters: [],
+    noStackTrace: true,
+    _: exercise ? [exercise] : undefined,
+  }, [
+    path.resolve('.')
+  ], ({ testResults }) => {
+    const results = {}
 
-  testResults.forEach(testResult => {
-    const exerciseName = testResult.testFilePath.match(/(\d+)\.spec\.js/)[1]
+    testResults.forEach(testResult => {
+      const exerciseName = testResult.testFilePath.match(/(\d+)\.spec\.js/)[1]
 
-    results[exerciseName] = {
-      failureMessage: testResult.failureMessage
-    }
+      results[exerciseName] = {
+        failureMessage: testResult.failureMessage
+      }
+    })
   })
+}, 100)
+
+fs.watch(path.resolve('src/exercises'), (eventType, filename) => {
+  runTests(filename.match(/(\d+)/)[0])
 })
